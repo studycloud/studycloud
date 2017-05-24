ee21eeef6f8460bc4ddc4b1065d408a98068d542
GiselleSerate <giselle.serate@gmail.com>
2017-05-24 16:45:37 -0700

    Well, now the box right aligns properly.

--- Design/HTML/header.js
+++ Design/HTML/header.js
@@ -6,8 +6,8 @@ function dropLogIn() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
+  alert(".logIn: "+event.target.matches('.logInF'))                                               // DEBUG LINE
   if (!event.target.matches('.logInContent') && !event.target.matches('#logInButton')) {
-    alert(event.target.Id)
     var dropdowns = document.getElementsByClassName("logInContent");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
