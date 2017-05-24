039b4aad7123b58dd65e469cfe201815d5a0c1fb
GiselleSerate <giselle.serate@gmail.com>
2017-05-24 11:54:41 -0700

    Working on login dropdown. Dialog box opens but closing it is buggy and therefore currently commented out.

---
+++ Design/HTML/header.js
@@ -0,0 +1,20 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropLogIn() {
+    document.getElementById("logInHidden").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.logInHidden')) {
+
+//     var dropdowns = document.getElementsByClassName("logInContent");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
