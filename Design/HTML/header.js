3d86ef1d6aa162a993ff20cbc3e305278029b674
GiselleSerate <giselle.serate@gmail.com>
2017-05-24 17:02:12 -0700

    Not much progress since last time. Major steps looking forwarad: make it so the box hides consistently.

--- Design/HTML/header.js
+++ Design/HTML/header.js
@@ -6,8 +6,9 @@ function dropLogIn() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  alert(".logIn: "+event.target.matches('.logInF'))                                               // DEBUG LINE
-  if (!event.target.matches('.logInContent') && !event.target.matches('#logInButton')) {
+  var logIn = document.getElementById("logIn");
+  alert("wassup: "+document.getElementById("logIn").is(event.target))                                               // DEBUG LINE
+  if (!logIn.is(event.target)) {
     var dropdowns = document.getElementsByClassName("logInContent");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
@@ -18,3 +19,20 @@ window.onclick = function(event) {
     }
   }
 }
+
+
+// // I know you shouldn't need to do this kind of thing bc git but I'm doing it anyway
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   alert(".logIn: "+event.target.matches('.logInF'))                                               // DEBUG LINE
+//   if (!event.target.matches('.logInContent') && !event.target.matches('#logInButton')) {
+//     var dropdowns = document.getElementsByClassName("logInContent");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
