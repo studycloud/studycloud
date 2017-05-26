db36d121759f566a7d03255925c866643a904370
GiselleSerate <giselle.serate@gmail.com>
2017-05-26 12:01:55 -0700

    jQuery made everything way cleaner, but it still hides when you click in the fields.

--- Design/HTML/header.js
+++ Design/HTML/header.js
@@ -1,38 +1,7 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function dropLogIn() {
-    document.getElementById("logInHidden").classList.toggle("show");
-}
+// Right, jQuery is a thing.
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  var logIn = document.getElementById("logIn");
-  alert("wassup: "+document.getElementById("logIn").is(event.target))                                               // DEBUG LINE
-  if (!logIn.is(event.target)) {
-    var dropdowns = document.getElementsByClassName("logInContent");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-
-// // I know you shouldn't need to do this kind of thing bc git but I'm doing it anyway
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//   alert(".logIn: "+event.target.matches('.logInF'))                                               // DEBUG LINE
-//   if (!event.target.matches('.logInContent') && !event.target.matches('#logInButton')) {
-//     var dropdowns = document.getElementsByClassName("logInContent");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
+$(document).ready(function(){
+    $("#logIn").click(function(){
+        $("#logInHidden").toggle();
+    });
+});
