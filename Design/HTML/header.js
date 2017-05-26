be35a8ed98c92a451de9759a8827f7c9384970b2
GiselleSerate <giselle.serate@gmail.com>
2017-05-26 12:45:59 -0700

    Clicks are logged super weirdly and I'm getting multiple clicks every time I tap the login button to bring up the dialog. It shows and immediately hides.

--- Design/HTML/header.js
+++ Design/HTML/header.js
@@ -1,7 +1,11 @@
-// Right, jQuery is a thing.
-
-$(document).ready(function(){
-    $("#logInButton").click(function(){
-        $("#logInHidden").toggle();
-    });
+// Right, jQuery is a thing. 
+$(document).ready(function(){ 
+	$("#logInButton").click(function(){ // Show/hide if you click on button. 
+		$("#logInHidden").show(1000);
+	});
+	$("body").on("click", ":not(#logInButton, #logInHidden, #logInHidden *)", function(){ 
+		// if ( $("#logInHidden").css('display') != 'none' ){
+			$("#logInHidden").hide(1000); 
+		// }
+	});
 });
