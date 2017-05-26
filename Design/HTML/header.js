e766a668ca9497af6759cfe1685d03f0f7670c92
GiselleSerate <giselle.serate@gmail.com>
2017-05-26 13:19:46 -0700

    Haven't fixed it, but I added a suggestion to the js file to fix it.

--- Design/HTML/header.js
+++ Design/HTML/header.js
@@ -1,11 +1,18 @@
 // Right, jQuery is a thing. 
 $(document).ready(function(){ 
+
 	$("#logInButton").click(function(){ // Show/hide if you click on button. 
 		$("#logInHidden").show(1000);
 	});
+
 	$("body").on("click", ":not(#logInButton, #logInHidden, #logInHidden *)", function(){ 
-		// if ( $("#logInHidden").css('display') != 'none' ){
+		if ( $("#logInHidden").css('display') != 'none' ){
 			$("#logInHidden").hide(1000); 
-		// }
+		}
 	});
+
 });
+
+
+// Attempting to make dialog hide when you click outside. Currently, shows and immediately hides. I think click events aren't separate. 
+// if this doesn't work change the show in line 5 to a toggle and take out lines 8-12 and everything should be fine. 
