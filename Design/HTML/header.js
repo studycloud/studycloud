65204a731a620058b3cfbf0923328e081dcadc08
Giselle Serate <giselle.serate@gmail.com>
2017-06-03 10:25:58 -0700

    Now handles clicking in fields gracefully.

--- Design/HTML/header.js
+++ Design/HTML/header.js
@@ -6,17 +6,9 @@ $(document).ready(function(){
 		$("#logInHidden").toggle(1000);
 	});
 
-	$("*:not(#logInButton, #logInButton *)").click(function(event){ // Show/hide dialog if you click on the log in button. 
+	$("*:not(#logInHidden).find(*), *:not(#logInButton).find(*)").click(function(event){ // Show/hide dialog if you click on the log in button. 
 		event.stopPropagation();
 		$("#logInHidden").hide(1000);
 	});
-
-
-	// $(document).keypress(function(event){ // Show/hide if you click on button. 
-	// 	if(event.which == 27){
-	// 		$("#logInHidden").hide(1000);
-	// 	}
-	// 	event.stopPropagation();
-	// });
 
 });
