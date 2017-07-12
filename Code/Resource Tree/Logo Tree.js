5a19a7768f61c1aeaabdde28721ec079c4594775
Maxwellfire <max@levymeister.com>
2017-07-11 22:44:16 -0700

    Remove old code from bottom.

--- Code/Resource Tree/Logo Tree.js
+++ Code/Resource Tree/Logo Tree.js
@@ -1,5 +1,3 @@
-
-//var graph = {"nodes": [], "links": [] };
 var LogoSVG = d3.select("#svg5045");
 
 var LogoData = 
@@ -207,118 +205,22 @@ function Stopper()
 }	
 
 function GraphUpdate()
-	{
+{
+	
+	Stars
+		.attr('cx', function(d) { return d.x; })
+		.attr('cy', function(d) { return d.y; });
+
+	Lines
+		.attr('d', function(d) { string = "M " + d.source.x + "," + d.source.y + " " + d.target.x + "," + d.target.y; return string })
 		
-		Stars
-			.attr('cx', function(d) { return d.x; })
-		    .attr('cy', function(d) { return d.y; });
+	Halos
+		.each(function(d, i, nodes) 
+			{ 
+				this.transform.baseVal[0].matrix.e = d.star.x + d.OffsetX;
+				this.transform.baseVal[0].matrix.f = d.star.y + d.OffsetY;
+				
+			}
+		);
 	
-		Lines
-			.attr('d', function(d) { string = "M " + d.source.x + "," + d.source.y + " " + d.target.x + "," + d.target.y; return string })
-			
-		Halos
-			.each(function(d, i, nodes) 
-				{ 
-					this.transform.baseVal[0].matrix.e = d.star.x + d.OffsetX;
-					this.transform.baseVal[0].matrix.f = d.star.y + d.OffsetY;
-					
-				}
-		    );
-		
-	}
-
-
-// for(i=0; i<NodesNum; i++)
-// {	
-	// graph["nodes"].push({id: i});
-	// graph["links"].push({target:Math.floor(Math.random() * NodesNum) , source: i});
-// }
-
-
-// var Width = window.innerWidth,
-    // Height = window.innerHeight,
-	// center = {x: Width/2, y: Height/2},
-	// scale = 1;
-
-
-
-// var GraphFrameInner = GraphSVG.append("g");
-
-// var link =  GraphFrameInner
-	// .selectAll('.link')
-    // .data(graph.links)
-    // .enter()
-		// .append('line')
-			// .attr('class', 'link')
-			
-// var node =  GraphFrameInner
-	// .selectAll('.node')
-    // .data(graph.nodes)
-    // .enter()
-		// .append('circle')
-		// .attr('class', 'node')
-		// .attr('r', function()
-			// {
-				// return (Width/100) * (Math.random() * 2 +0.5);
-			// })
-		// .style("fill", "magenta")
-		// .on("click", CenterNode);
-
-
-// We're about to tell the force layout to start its
-// calculations. We do, however, want to know when those
-// calculations are complete, so before we kick things off
-// we'll define a function that we want the layout to call
-// once the calculations are done.
-
-// var SimulationForce = d3.forceSimulation()
-    // .force("ForceLink", d3.forceLink())
-    // .force("ForceCharge", d3.forceManyBody())
-    // .force("ForceCenter", d3.forceCenter(Width / 2, Height / 2));
-
-// SimulationForce
-	// .nodes(graph.nodes)
-	// .on('tick', GraphUpdate);
-
-// SimulationForce.force("ForceLink")
-	// .links(graph.links)
-	// .strength(.3);
-	
-// SimulationForce.force("ForceCharge")
-	// .strength(-50);
-	
-// function CenterNode(Node)
-	// {
-		// var TranslateX
-		// var TranslateY
-		
-		// if (d3.event.ctrlKey) 
-			// {
-				// scale = 1
-				// TranslateX = 0
-				// TranslateY = 0
-			// }
-		// else
-			// {
-				// scale = scale * 4
-				// TranslateX = center.x - Node.x * scale;
-				// TranslateY = center.y - Node.y * scale;
-			// }
-			
-		 // GraphFrameInner.transition()
-			// .duration(700)
-			// .attr("transform", "translate("+ TranslateX + "," + TranslateY  + ")scale(" + scale +")");
-	// }
-
-// function GraphUpdate()
-	// {
-		// node
-			// .attr('cx', function(d) { return d.x; })
-		    // .attr('cy', function(d) { return d.y; });
-	
-		// link
-			// .attr('x1', function(d) { return d.source.x })
-			// .attr('y1', function(d) { return d.source.y  })
-			// .attr('x2', function(d) { return d.target.x  })
-			// .attr('y2', function(d) { return d.target.y  });
-	// }
+}
