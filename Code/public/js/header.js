6057bc3cc37599dd548e27b9c25a8fcf8231cbaf
Giselle Serate <giselle.serate@gmail.com>
2017-11-18 20:02:18 -0800

    Finish this iteration of the site design.

--- Code/public/js/header.js
+++ Code/public/js/header.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 		$("#logInHidden").toggle(1000);
 	});
 
+	// TODO: Real freakin buggy. I'll come back and either fix this or refactor and delete it. 
 	// $("*:not(#logInHidden).find(*), *:not(#logInButton).find(*)").click(function(event){ // Show/hide dialog if you click on the log in button. 
 	// 	event.stopPropagation();
 	// 	$("#logInHidden").hide(1000);
