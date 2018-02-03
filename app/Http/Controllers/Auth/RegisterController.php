3a073c44f2c363e4972fa377d0f4c080b83dc352
Arya Massarat <aryam7@gmail.com>
2018-02-02 22:11:36 -0800

    started work on user registration
    (cherry picked from commit 8eb28b47671c0008c1f255ef4d29afbc6e34172e)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,9 +48,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'fname' => 'required|max:255',
+            'lname' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
+            'type' => 'in:student,teacher'
         ]);
     }
 
@@ -66,6 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'type' => $data['type']
         ]);
     }
 }
