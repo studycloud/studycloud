6d2011edcc8db442b839506b978af36a50a1f115
TrangQDinh <quynhtrang0504@gmail.com>
2018-02-17 21:23:10 -0800

    Continue working on  .getData function

--- Code/Resource Tree/Server.js
+++ Code/Resource Tree/Server.js
@@ -12,9 +12,8 @@ function Server(node, handleError, handleSuccess)
 Server.prototype.getData = function(node, levels)
 {
     var self = this;
-    var url = "/tree/data/?topic="+node+"&levels="+levels;
+    var url = "tree/data/?topic"+node+"&levels="+levels;
     return d3.json(url);
-
 
 };
 
