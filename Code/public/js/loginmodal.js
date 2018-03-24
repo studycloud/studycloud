0975be94bc94e995491bddcad293f18ec887c4ef
Giselle Serate <giselle.serate@gmail.com>
2018-03-23 21:28:24 -0700

    Fix checkbox in issue #47

--- Code/public/js/loginmodal.js
+++ Code/public/js/loginmodal.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == document.getElementById('myModal')) {
+        document.getElementById('myModal').style.display = "none";
     }
 }
