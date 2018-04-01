303817bb559e09e80f82193217d1c28456b6ef9c
Giselle Serate <giselle.serate@gmail.com>
2018-03-31 21:16:49 -0700

    Restructure resource viewer to include a callback

--- Code/public/js/resource_viewer.js
+++ Code/public/js/resource_viewer.js
@@ -24,37 +24,42 @@
 // xmlhttp.send();
 
 // Dummy data which I would get from the server.
-var received = '{"meta": {"name": "Resource 1", "author_name": "Giselle Serate", "author_type": "teacher"}, "contents": [ {"name": "Resource Content 1", "type": "link", "content": "google.com", "created": "date", "updated": "date"}]}'
-
-var resource = JSON.parse(received);
+var received = '{"meta": {"name": "Resource 1", "author_name": "Giselle Serate", "author_type": "teacher"}, "contents": [ {"name": "Resource Content 1", "type": "link", "content": "google.com", "created": "date", "updated": "date"}]}';
 
 $(document).ready(function(){ 
-	document.getElementById('resource-name').innerHTML=resource.meta.name;
-	set_author();
-	for(var i=0;i<1;i++)
-	{
-		display_content(i);
-	}
+	callback(received);
 });
 
+// Callback function that server will give the data.
+function callback(received)
+{
+	var resource = JSON.parse(received);
+	document.getElementById('resource-name').innerHTML=resource.meta.name;
+	set_author(resource.meta.author_name, resource.meta.author_type);
+	for(var i=0;i<1;i++)
+	{
+		display_content(i, resource.contents[i]);
+	}
+}
+
 // Set author and classes to format. 
-function set_author() 
+function set_author(name, type) 
 {
 	// Clear all classes on the author-name field. 
 	var cl=document.getElementById('author-name').classList;
 	for(var i=cl.length; i>0; i--) {
 	    cl.remove(cl[0]);
 	}
-	document.getElementById('author-name').classList.add(resource.meta.author_type);
-	document.getElementById('author-name').innerHTML=resource.meta.author_name;
+	document.getElementById('author-name').classList.add(type);
+	document.getElementById('author-name').innerHTML=name;
 }
 
-// Display one of the contents in the array.
-function display_content(which)
+// Display one of the content elements in the array.
+function display_content(num, element)
 {
-	if(resource.contents[which].type="link")
+	if(element.type="link")
 	{
-		document.getElementById('content-0').innerHTML="<a href="+resource.contents[which].content+">"+resource.contents[which].name+"</a>";
+		document.getElementById('content-'+num).innerHTML="<a href="+element.content+">"+element.name+"</a>";
 	}
 	// Add other types as you will. 
 }
