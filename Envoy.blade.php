d41e3e7fe75c47edc5a4ab925ff5e197df48c87c
Arya Massarat <aryam7@gmail.com>
2018-04-07 21:55:15 -0700

    finished draft of deployment script

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,10 @@
 @servers(['web' => 'studycloud'])
 
-@task('deploy', ['on' => 'web'])
+@task('deploy', ['on' => 'web', 'confirm' => true])
 	cd ~/beta
-	cat public/index.html
+	php artisan down
+	git pull origin production
+	composer install --no-dev --prefer-dist
+	php artisan migrate
+	php artisan up
 @endtask
