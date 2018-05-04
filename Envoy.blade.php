f32bb910de00a7b01354a63a1907bc35a5eb7b4f
Arya Massarat <aryam7@gmail.com>
2018-05-03 21:53:21 -0700

    added deploy command to remote server instead of in envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,10 +1,5 @@
 @servers(['web' => 'studycloud'])
 
 @task('deploy', ['on' => 'web', 'confirm' => true])
-	cd ~/beta
-	php artisan down
-	git pull origin production
-	composer install --no-dev --prefer-dist
-	php artisan migrate
-	php artisan up
+	deploy
 @endtask
