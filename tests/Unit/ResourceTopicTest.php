dd988b8245c9bbe9f231048f049c693abd453c5b
Arya Massarat <aryam7@gmail.com>
2018-06-02 23:20:56 -0700

    commented ResourceTopicTest

--- tests/Unit/ResourceTopicTest.php
+++ tests/Unit/ResourceTopicTest.php
@@ -35,7 +35,7 @@ class ResourceTopicTest extends TestCase
 	 * a given Topic in the tree.
 	 * @param  Topic|null 	$topic     	the root of the subtree on which to recurse (or null if the Topic Tree's root)
 	 * @param  Collection	$resources 	a collection of resource IDs that have already occurred in $topic or its ancestors
-	 * @return Collection 				a collection of resource IDs that have appeared more than once
+	 * @return Collection				a collection of resource IDs that have appeared more than once
 	 */
 	private function invalidResources(Topic $topic = null, $resources)
 	{
@@ -48,18 +48,28 @@ class ResourceTopicTest extends TestCase
 		{
 			$children = $topic->children;
 		}
-
+		// iterate through each child of $topic and call invalidResources on
+		// them, merging the results with a collection of resources that were
+		// both in $resources and the child's resources
 		return $children->map(
 			function ($child) use ($resources)
 			{
+				// get this child's resources as a collection of IDs
 				$child_resources = $child->resources->pluck('id');
+				// create a collection of resources that are both in $resources
+				// and in $child_resources using intersect(), then merge the
+				// result with this child's invalid resources
 				return $resources->intersect($child_resources)->merge(
+					// recursion! remember to merge $resources and 
+					// $child_resources, since we want to build up a growing
+					// collection of resources that have occurred on our way
+					// down the tree
 					$this->invalidResources(
 						$child,
 						$resources->merge($child_resources)
 					)
-				)->all();
+				)->all(); // use all() to convert to a standard array
 			}
-		)->collapse()->unique();
+		)->collapse()->unique(); // use collapse() to flatten the arrays
 	}
 }
