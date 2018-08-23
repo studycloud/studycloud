5679059bad3617e9afaab3039f0baec7b65e487b
Arya Massarat <aryam7@gmail.com>
2018-08-23 08:10:59 -0700

    add new class migration and seeder files

---
+++ database/seeds/ClassParentTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClassParentTableSeeder extends Seeder
+{
+	/**
+	 * What is the maximum number of classes that each level of the tree can have?
+	 */
+	const NUM_MAX_CLASSES = 4;
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
