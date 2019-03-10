da314c3a08e458ea192585f8c35bc63266e0c188
Arya Massarat <aryam7@gmail.com>
2019-03-10 11:37:36 -0700

    allow class seeding to create sticks if desired

--- database/seeds/ClassParentTableSeeder.php
+++ database/seeds/ClassParentTableSeeder.php
@@ -52,7 +52,10 @@ class ClassParentTableSeeder extends Seeder
 	{
 		// how many classes should be at this level of the tree?
 		// keep choosing a random number until the choice isn't 1
-		while (is_null($num_classes_level) || $num_classes_level == 1)
+		while (
+			is_null($num_classes_level) ||
+			($num_classes_level == 1 && self::NUM_MAX_CLASSES != 1)
+		)
 		{
 			$num_classes_level = rand( $num_classes_level_min, min(self::NUM_MAX_CLASSES, $this->classes->count()) );
 		}
