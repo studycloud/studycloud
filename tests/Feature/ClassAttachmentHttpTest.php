7ce5cd5f44bcab0e1d1762d67bd62822182e50ac
Arya Massarat <aryam7@gmail.com>
2019-03-19 18:20:16 -0700

    start testing class attachment (see #136)

---
+++ tests/Feature/ClassAttachmentHttpTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ClassAttachmentHttpTest extends TestCase
+{
+	/**
+	 * A basic test example.
+	 *
+	 * @return void
+	 */
+	public function testExample()
+	{
+		$this->assertTrue(true);
+	}
+
+	public function createTree()
+	{
+		// create a tree under a single node and add it to the database
+	}
+}
