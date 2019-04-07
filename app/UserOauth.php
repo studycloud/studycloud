50a76a5bc4969a57c383512f7591d0a54b4712ac
Arya Massarat <aryam7@gmail.com>
2019-04-06 19:07:55 -0700

    add UserOauth model to access a user's oauth meta data

---
+++ app/UserOauth.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class UserOauth extends Model
+{
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'user_oauth';
+
+	/**
+	 * the primary key is the user_id and the type of oauth info
+	 * @var array
+	 */
+	protected $primaryKey = ['user_id', 'type'];
+
+	/**
+	 * the primary key cannot be incremented
+	 * @var boolean
+	 */
+	public $incrementing = false;
+
+	/**
+	 * return the user who owns this oauth data
+	 */
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	/**
+	 * Set the keys for a save update query.
+	 *
+	 * @param  \Illuminate\Database\Eloquent\Builder  $query
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
+	protected function setKeysForSaveQuery(Builder $query)
+	{
+		$keys = $this->getKeyName();
+		if(!is_array($keys)){
+			return parent::setKeysForSaveQuery($query);
+		}
+
+		foreach($keys as $keyName){
+			$query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+		}
+
+		return $query;
+	}
+
+	/**
+	 * Get the primary key value for a save query.
+	 *
+	 * @param mixed $keyName
+	 * @return mixed
+	 */
+	protected function getKeyForSaveQuery($keyName = null)
+	{
+		if(is_null($keyName)){
+			$keyName = $this->getKeyName();
+		}
+
+		if (isset($this->original[$keyName])) {
+			return $this->original[$keyName];
+		}
+
+		return $this->getAttribute($keyName);
+	}
+}
