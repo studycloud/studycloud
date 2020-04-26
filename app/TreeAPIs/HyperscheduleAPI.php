fc07303b5870345aef495a5f571f52a578f044f3
Arya Massarat <aryam7@gmail.com>
2020-04-25 18:38:53 -0700

    start handling class import with hyperschedule

--- app/TreeAPIs/HyperscheduleAPI.php
+++ app/TreeAPIs/HyperscheduleAPI.php
@@ -4,6 +4,7 @@ namespace App\TreeAPIs;
 
 use GuzzleHttp\Client;
 use App\TreeAPIs\TreeAPI;
+use Illuminate\Support\Facades\Validator;
 
 class HyperscheduleAPI implements TreeAPI
 {
@@ -28,12 +29,35 @@ class HyperscheduleAPI implements TreeAPI
 	 */
 	public function update()
 	{
+		// make the request to their API
 		$res = $this->client->request('GET', $this->url, [
 			'query' => [
 				'school' => 'hmc'
 			]
 		]);
-		return $res;
+		// validate the response! <-- important to make sure the data doesn't have anything sneaky in it!
+		$this->validate($res);
+		// now check that there wasn't an error
+		if ($res->getStatusCode() == 200 || !is_null($res->getBody()['error']))
+		{
+			abort($res->getStatusCode(), $res->getBody()['error']);
+		}
+		// convert the class data to a format we can work with
+		$classes = $this->toClasses($res->getBody());
+		// tell the user if it worked
 		return $res->getBody();
 	}
+
+	protected function validate($response)
+	{
+		$validator = Validator::make($res->getBody(), [
+
+		]);
+	}
+
+	protected function toClasses($value='')
+	{
+		# code...
+	}
+
 }
