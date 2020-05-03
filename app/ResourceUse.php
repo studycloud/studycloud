a75467a512befbad0e27d48823d579cabea72eb3
Arya Massarat <23412689+aryam7@users.noreply.github.com>
2020-05-03 11:21:29 -0700

    create author relationship for ResourceUses

--- app/ResourceUse.php
+++ app/ResourceUse.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\User;
 use App\Resource;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,7 +20,7 @@ class ResourceUse extends Model
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['name', 'author_id'];
+	protected $fillable = ['name'];
 
 	/**
 	 * define the many-to-one relationship between resources and their use
@@ -28,5 +29,14 @@ class ResourceUse extends Model
 	public function resources()
 	{
 		return $this->hasMany(Resource::class, 'use_id');
+	}
+
+	/**
+	 * define the many-to-one relationship between resources and their author
+	 * @return User	the author of this resource
+	 */
+	public function author()
+	{
+		return $this->belongsTo(User::class);
 	}
 }
