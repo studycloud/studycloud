2b28afd8bf87b6bbf1b303b3f5d95c6fb706f27d
Arya Massarat <aryam7@gmail.com>
2020-05-02 19:21:25 -0700

    change redirect behavior when user is not signed in

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -60,6 +60,8 @@ class Handler extends ExceptionHandler
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
-        return redirect()->guest('login');
+        // TODO: fix /login so that it redirects to the defualt provider
+        // and then change this to /login
+        return redirect()->guest('/login/google');
     }
 }
